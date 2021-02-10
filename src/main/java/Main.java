c39d189bbf1b4598799b9f4b94d56eb5a9a1d7d3
shadisab <shadisabbagh40@gmail.com>
2021-02-10 23:34:55 +0200

    Initial program

---
+++ src/main/java/Main.java
@@ -0,0 +1,59 @@
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ClientChannel;
+import org.apache.sshd.client.channel.ClientChannelEvent;
+import org.apache.sshd.client.session.ClientSession;
+
+import java.io.*;
+import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+        String host="172.105.72.154";
+        String user="root";
+        String password="Shad!2951995";
+        String command1="ls MainDir";
+        try{
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+            Session session=jsch.getSession(user, host, 22);
+            session.setPassword(password);
+            session.setConfig(config);
+            session.connect();
+            System.out.println("Connected");
+
+            Channel channel=session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command1);
+            channel.setInputStream(null);
+            ((ChannelExec)channel).setErrStream(System.err);
+
+            InputStream in=channel.getInputStream();
+            channel.connect();
+            byte[] tmp=new byte[1024];
+            while(true){
+                while(in.available()>0){
+                    int i=in.read(tmp, 0, 1024);
+                    if(i<0)break;
+                    System.out.print(new String(tmp, 0, i));
+                }
+                if(channel.isClosed()){
+                    System.out.println("exit-status: "+channel.getExitStatus());
+                    break;
+                }
+                try{Thread.sleep(1000);}catch(Exception ee){}
+            }
+            channel.disconnect();
+            session.disconnect();
+            System.out.println("DONE");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
